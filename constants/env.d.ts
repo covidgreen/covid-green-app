11507bf85a69ca98f89b0c1b154c3f3da05bb075
David J Harding <dharding4@bloomberg.net>
2020-07-31 16:53:44 -0400

    Declared types for dotenv to avoid typescript errors

---
+++ constants/env.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-native-dotenv' {
+  export const API_HOST: string;
+  export const BUILD_VERSION: string;
+  export const ENV: string;
+  export const HIDE_DEBUG: string;
+  export const SAFETYNET_KEY: string;
+  export const TEST_TOKEN: string;
+}
