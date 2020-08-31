3b91c7386970316ca13ceffe3f49e7b94d42c7cb
Jack Murdoch <jack.murdoch@nearform.com>
2020-08-31 13:02:51 +0100

    Link react-native-device-info

--- android/app/src/main/java/gov/ny/health/proximity/MainApplication.java
+++ android/app/src/main/java/gov/ny/health/proximity/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.pedrouid.crypto.RCTCryptoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
